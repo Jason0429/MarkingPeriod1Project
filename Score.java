2ab4ecb62a8ba66a3d25119dbf27e0441dc8e0b0
Jason Cheung <jasoncheung0429@gmail.com>
2020-11-15 21:49:58 -0500

    Presentation

--- Score.java
+++ Score.java
@@ -1,17 +1,19 @@
 public class Score {
     private int currentScore;
-    private static int min = Integer.MIN_VALUE;
-    private static int max = Integer.MAX_VALUE;
+    private static int min;
+    private static int max;
 
     public Score() {
         currentScore = 0;
+        max = Integer.MAX_VALUE;
+        min = Integer.MIN_VALUE;
     }
 
-    public double getScore() {
+    public int getScore() {
         return currentScore;
     }
 
-    public static int getRandomInt() {
+    public int getRandomInt() {
         return (int) Math.sqrt(Math.pow(Math.abs((Math.random() * (max - min + 1) + min)), 1));
     }
     
